231b041c321eea88ad06012423b1569602a1b63e
Ha Thu <tranhathu6989@gmail.com>
2023-10-29 21:40:52 +0100

    Add hide or show password

--- client/src/components/auth/Login/Login.js
+++ client/src/components/auth/Login/Login.js
@@ -2,13 +2,13 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../../../context/auth.context";
-import Header from "../../../common/header/Header";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   const { storeToken, authenticateUser } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
 
@@ -16,16 +16,12 @@ function Login() {
     e.preventDefault();
 
     const requestBody = { email, password };
-    console.log("requestBody", requestBody);
     axios
       .post(`${process.env.REACT_APP_API_URL}/user/login`, requestBody)
       .then((response) => {
         const jwt = response.data.authToken;
-        console.log("Login was sucessful. JWT token: ", jwt);
-
         storeToken(jwt);
         authenticateUser();
-
         navigate("/");
       })
       .catch((error) => {
@@ -36,60 +32,65 @@ function Login() {
   };
 
   return (
-    <>
-      <Header />
-      <div className="container-auth">
-        <div className="title-auth">
-          <h1>Login</h1>
-        </div>
-        <div className="form-auth">
-          {errorMessage && <h4 className="error">{errorMessage} !</h4>}
-          <form onSubmit={handleLoginSubmit}>
-            <div className="box-label-input login">
-              <label>Email</label>
-              <input
-                type="email"
-                placeholder="email@gmail.com"
-                name="email"
-                required={true}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="box-label-input login">
-              <label>Password</label>
-              <input
-                type="password"
-                placeholder="*********"
-                name="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button variant="success" type="submit" className="my-2">
-              Login
+    <div className="container-auth">
+      <div className="title-auth">
+        <h1>Login</h1>
+      </div>
+      <div className="form-auth">
+        {errorMessage && <h4 className="error">{errorMessage} !</h4>}
+        <form onSubmit={handleLoginSubmit}>
+          <div className="box-label-input login">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="email@gmail.com"
+              name="email"
+              required={true}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="box-label-input input-password login">
+            <label>Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="*********"
+              name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <a
+              onClick={() => setShowPassword(!showPassword)}
+              className={
+                showPassword
+                  ? " pass-control-view show-pass"
+                  : " pass-control-view hide-pass"
+              }
+            />
+          </div>
+          <button variant="success" type="submit" className="my-2">
+            Login
+          </button>
+        </form>
+        <div className="return return-login-signup">
+          <p>N'avez-vous pas un compte?</p>
+          <Link to="/signup">
+            <button variant="info" size="sm" className="ml-2">
+              Signup
             </button>
-          </form>
-          <div className="return return-login-signup">
-            <p>N'avez-vous pas un compte?</p>
-            <Link to="/signup">
-              <button variant="info" size="sm" className="ml-2">
-                Signup
-              </button>
-            </Link>
-          </div>
-          <div className="return return-home">
-            <p>Retour à l'accueil</p>
-            <Link to="/">
-              <button variant="info" size="sm" className="ml-2">
-                Accueil
-              </button>
-            </Link>
-          </div>
+          </Link>
+        </div>
+        <div className="return return-home">
+          <p>Retour à l'accueil</p>
+          <Link to="/">
+            <button variant="info" size="sm" className="ml-2">
+              Accueil
+            </button>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- client/src/components/auth/Signup/Signup.js
+++ client/src/components/auth/Signup/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import Header from "../../../common/header/Header";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -10,13 +9,14 @@ function Signup() {
   const [address, setAddress] = useState("");
   const [numberPhone, setNumberPhone] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
 
   const handleSignupSubmit = (e) => {
     e.preventDefault();
 
-    const requestBody = { email, password };
+    const requestBody = { email, password, username, address, numberPhone };
 
     axios
       .post(`${process.env.REACT_APP_API_URL}/user/signup`, requestBody)
@@ -32,90 +32,95 @@ function Signup() {
   };
 
   return (
-    <>
-      <Header />
-      <div className="container-auth">
-        <div className="title-auth">
-          <h1>Signup</h1>
-        </div>
-        <div className="form-auth">
-          {errorMessage && <h4 className="error">{errorMessage} !</h4>}
-          <form onSubmit={handleSignupSubmit}>
-            <div className="box-label-input signup">
-              <label>Email</label>
-              <input
-                type="email"
-                placeholder="email@gmail.com"
-                name="email"
-                required={true}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="box-label-input signup">
-              <label>Nom</label>
-              <input
-                type="text"
-                placeholder="DEVILLIERS Lucie"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="box-label-input signup">
-              <label>Password</label>
-              <input
-                type="password"
-                placeholder="*********"
-                name="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="box-label-input signup">
-              <label>Adresse</label>
-              <input
-                type="text"
-                placeholder="123 Avenue Joseph, 91120 PALAISEAU"
-                name="address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </div>
-            <div className="box-label-input signup">
-              <label>N° de téléphone</label>
-              <input
-                type="text"
-                placeholder="+33 695396189"
-                name="numberPhone"
-                value={numberPhone}
-                onChange={(e) => setNumberPhone(e.target.value)}
-              />
-            </div>
-            <button variant="success" type="submit" className="my-2">
-              Signup
+    <div className="container-auth">
+      <div className="title-auth">
+        <h1>Signup</h1>
+      </div>
+      <div className="form-auth">
+        {errorMessage && <h4 className="error">{errorMessage} !</h4>}
+        <form onSubmit={handleSignupSubmit}>
+          <div className="box-label-input signup">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="email@gmail.com"
+              name="email"
+              required={true}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="box-label-input signup">
+            <label>Nom</label>
+            <input
+              type="text"
+              placeholder="DEVILLIERS Lucie"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="box-label-input input-password signup">
+            <label>Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="*********"
+              name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <a
+              onClick={() => setShowPassword(!showPassword)}
+              className={
+                showPassword
+                  ? " pass-control-view show-pass"
+                  : " pass-control-view hide-pass"
+              }
+            />
+          </div>
+          <div className="box-label-input signup">
+            <label>Adresse</label>
+            <input
+              type="text"
+              placeholder="123 Avenue Joseph, 91120 PALAISEAU"
+              name="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div className="box-label-input signup">
+            <label>N° de téléphone</label>
+            <input
+              type="text"
+              placeholder="+33 695396189"
+              name="numberPhone"
+              value={numberPhone}
+              onChange={(e) => setNumberPhone(e.target.value)}
+            />
+          </div>
+          <button variant="success" type="submit" className="my-2">
+            Signup
+          </button>
+        </form>
+        <div className="return return-login-signup">
+          <p>Avez-vous un compte?</p>
+          <Link to="/login">
+            <button variant="info" size="sm" className="ml-2">
+              Login
             </button>
-          </form>
-          <div className="return return-login-signup">
-            <p>Avez-vous un compte?</p>
-            <Link to="/login">
-              <button variant="info" size="sm" className="ml-2">
-                Login
-              </button>
-            </Link>
-          </div>
-          <div className="return return-home">
-            <p>Retour à l'accueil</p>
-            <Link to="/">
-              <button variant="info" size="sm" className="ml-2">
-                Accueil
-              </button>
-            </Link>
-          </div>
+          </Link>
+        </div>
+        <div className="return return-home">
+          <p>Retour à l'accueil</p>
+          <Link to="/">
+            <button variant="info" size="sm" className="ml-2">
+              Accueil
+            </button>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
